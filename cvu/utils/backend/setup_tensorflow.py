0cbcc6069a5d75269806275c04e67d8862324f96
shivamswarnkar <ss8464@nyu.edu>
2021-07-14 20:16:46 +0530

    refactor setup

--- cvu/utils/backend/setup_tensorflow.py
+++ cvu/utils/backend/setup_tensorflow.py
@@ -1,26 +1,7 @@
-import importlib
-import os
+import tensorflow as tf
+
+__version__ = tf.__version__
 
 
-def setup(device):
-    try:
-        attempt_import(device)
-        return
-    except ModuleNotFoundError:
-        os.system("pip install tensorflow" +
-                  ("" if device == "cpu" else "-gpu"))
-
-    try:
-        attempt_import(device)
-
-    except Exception as error:
-        print("[CVU-Error] Tensorflow Import Failed, either",
-              "change backend or reinstall it properly...")
-        print(error)
-
-
-def attempt_import(device):
-    tf = importlib.import_module("tensorflow")
-    if device != 'cpu' and not bool(tf.config.list_physical_devices('GPU')):
-        device = 'cpu'
-    print(f"[CVU-Info] Backend: Tensorflow-{tf.__version__}-{device}")
+def is_gpu_available():
+    return bool(tf.config.list_physical_devices('GPU'))
--- cvu/utils/backend/setup_tflite.py
+++ cvu/utils/backend/setup_tflite.py
@@ -1,23 +1,8 @@
-import importlib
-import os
+from sys import version
+import tflite
+
+__version__ = tflite.__version__
 
 
-def setup(device):
-    try:
-        attempt_import(device)
-        return
-    except ModuleNotFoundError:
-        os.system("pip install tflite")
-
-    try:
-        attempt_import(device)
-
-    except Exception as error:
-        print("[CVU-Error] TFLite Import Failed, either",
-              "change backend or reinstall it properly...")
-        print(error)
-
-
-def attempt_import(device):
-    tflite = importlib.import_module("tflite")
-    print(f"[CVU-Info] Backend: TFLite-{tflite.__version__}-{device}")
+def is_gpu_available():
+    return False
