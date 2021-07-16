48c7cad8569fc9ab0ed60fb8069c8e92e3f92c75
shivamswarnkar <ss8464@nyu.edu>
2021-07-16 21:23:44 +0530

    add documentation

--- cvu/preprocess/image/general.py
+++ cvu/preprocess/image/general.py
@@ -1,10 +1,46 @@
-def bgr_to_rgb(image):
+"""Contains genearl preprocess functions for images.
+ - BGR to RGB Images (bgr_to_rgb)
+ - Channels Last to Channels First (hwc_to_whc)
+ - Normalize Image (normalize)
+"""
+import numpy as np
+
+
+def bgr_to_rgb(image: np.ndarray) -> np.ndarray:
+    """Convert BGR to RGB image.
+    BGR = Blue-Green-Red
+    RGB = Red-Green-Blue
+
+    Args:
+        image (np.ndarray): BGR format image.
+
+    Returns:
+        np.ndarray: RGB format image.
+    """
     return image[..., ::-1]
 
 
-def hwc_to_whc(image):
+def hwc_to_whc(image: np.ndarray) -> np.ndarray:
+    """Convert channels-last to channels-first
+    channels-last = Height-Width-Channels
+    channels-first = Channels-Height-Width
+
+    Args:
+        image (np.ndarray): HWC format image.
+
+    Returns:
+        np.ndarray: CHW format image.
+    """
     return image.transpose(2, 0, 1)
 
 
-def normalize(image):
+def normalize(image: np.ndarray) -> np.ndarray:
+    """Normalize image pixels to be in the 0-1 range.
+
+    Args:
+        image (np.ndarray): Image with pixels in range 0-255
+
+    Returns:
+        np.ndarray: Normalized image with pixels in range 0-1
+    """
     return image / 255.0
