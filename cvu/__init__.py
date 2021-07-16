d1fa111f299c7e53d7ed0dda38f2f8cf24f6b0af
shivamswarnkar <ss8464@nyu.edu>
2021-07-16 20:44:24 +0530

    fix style issue

--- cvu/__init__.py
+++ cvu/__init__.py
@@ -1,12 +1,15 @@
 """CVU: Computer Vision Utils
-Easy to use and consistent interface across various Computer Vision Use cases such as Object Detection,
-Segmentation, Tracking etc, on various backends like TensorRT, TFLite, TF, Torch etc. This tool aims at
-making computer vision accessible to everyone with or without in-depth Computer Vision Knowledge.
-CVU already contains multiple state of the art generalized models for you to use right out of the box,
-but it also supports custom weights and model-architectures.
+Easy to use and consistent interface across various Computer Vision
+Use cases such as Object Detection, Segmentation, Tracking etc, on various
+backends like TensorRT, TFLite, TF, Torch etc. This tool aims at making
+computer vision accessible to everyone with or without in-depth Computer
+Vision Knowledge. CVU already contains multiple state of the art generalized
+models for you to use right out of the box, but it also supports custom weights
+and model-architectures.
 
-For example, following code snippet will perform Object Detection for all the coco classes and create an
-output video with box drawn. (CVU uses Yolov5s as default detector)
+For example, following code snippet will perform Object Detection for
+all the coco classes and create an output video with box drawn.
+(CVU uses Yolov5s as default detector)
 
 ```python
     from vidsz.opencv import Reader, Writer
