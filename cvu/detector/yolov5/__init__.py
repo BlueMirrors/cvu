83421fb74071a42d9c1aca2bae07c9bf006d8041
shivamswarnkar <ss8464@nyu.edu>
2021-07-29 17:12:05 +0530

    add yolov5-module level docstring

--- cvu/detector/yolov5/__init__.py
+++ cvu/detector/yolov5/__init__.py
@@ -1,1 +1,27 @@
+"""This module contains core Yolov5 Implementation with various
+functional backends.
+
+Find more about Yolov5 here from their official repository
+https://github.com/ultralytics/yolov5
+
+Example Usage
+
+    # import core
+    from cvu.detector.yolov5 import Yolov5
+
+    # create detector object
+    detector = Yolov5(classes='coco')
+
+    # load images BGR format
+    image = ...
+
+    # inference
+    predictions = detector(image)
+
+    # print results
+    print(predictions)
+
+    # draw results
+    predictions.draw(image)
+"""
 from .core import Yolov5
