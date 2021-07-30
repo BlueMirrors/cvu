cce59553d694eec7d0fbf541822d512643425ca8
shivamswarnkar <ss8464@nyu.edu>
2021-07-30 12:43:47 +0530

    fix style issues for np bbox utils, add docstring

--- cvu/utils/bbox.py
+++ cvu/utils/bbox.py
@@ -4,27 +4,37 @@ URL: https://github.com/ultralytics/yolov5/blob/master/utils/general.py
 import numpy as np
 
 
-def xywh2xyxy(x):
-    # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
-    y = np.copy(x)
-    y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
-    y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
-    y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
-    y[:, 3] = x[:, 1] + x[:, 3] / 2  # bottom right y
-    return y
+def xywh2xyxy(xywh: np.ndarray) -> np.ndarray:
+    """Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2]
 
+    Args:
+        xywh (np.ndarray): array of 4 float [center_x, center_y, width, height]
 
-def box_iou(box1, box2):
-    # Modified https://github.com/pytorch/vision/blob/master/torchvision/ops/boxes.py
-    """
-    Return intersection-over-union (Jaccard index) of boxes.
-    Both sets of boxes are expected to be in (x1, y1, x2, y2) format.
-    Arguments:
-        box1 (Tensor[N, 4])
-        box2 (Tensor[M, 4])
     Returns:
-        iou (Tensor[N, M]): the NxM matrix containing the pairwise
-            IoU values for every element in boxes1 and boxes2
+        np.ndarray: array of 4 float [x1, y1, x2, y2] where (x1,y1)==top-left
+        and (x2,y2)==bottom-right.
+    """
+    xyxy = np.copy(xywh)
+    xyxy[:, 0] = xywh[:, 0] - xywh[:, 2] / 2  # top left x
+    xyxy[:, 1] = xywh[:, 1] - xywh[:, 3] / 2  # top left y
+    xyxy[:, 2] = xywh[:, 0] + xywh[:, 2] / 2  # bottom right x
+    xyxy[:, 3] = xywh[:, 1] + xywh[:, 3] / 2  # bottom right y
+    return xyxy
+
+
+def box_iou(box1: np.ndarray, box2: np.ndarray) -> np.ndarray:
+    """Return intersection-over-union (Jaccard index) of boxes.
+    Both sets of boxes are expected to be in (x1, y1, x2, y2) format.
+
+    # Modified https://github.com/pytorch/vision/blob/master/torchvision/ops/boxes.py
+
+    Args:
+        box1 (np.ndarray[N, 4]): first box
+        box2 (np.ndarray[M, 4]): second box
+
+    Returns:
+        iou (np.ndarray[N, M]): the NxM matrix containing the pairwise IoU values for
+        every element in boxes1 and boxes2
     """
     def box_area(box):
         # box = 4xn
