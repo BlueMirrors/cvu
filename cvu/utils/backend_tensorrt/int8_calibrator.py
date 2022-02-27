523b82ace170ed73f04bfc6363c30a0b9c56a17f
makaveli10 <suryanvineet47@gmal.com>
2022-02-27 14:44:03 +0530

    :hammer: :hammer: refactor

--- cvu/utils/backend_tensorrt/int8_calibrator.py
+++ cvu/utils/backend_tensorrt/int8_calibrator.py
@@ -1,83 +1,78 @@
-import tenosrrt as trt
-import pycuda.driver as cuda
-import pycuda.autoinit
-import numpy as np
-import cv2
+"""This file contains TensorRT's trt.IInt8EntropyCalibrator2 implementation.
+This calibrator (tensorRT-backend) performs int8 calibration using TensorRT,
+on a given set of images, and returns builds the TensorRT engine after this
+calibration process is completed.
+"""
 
-from cvu.preprocess.image.letterbox import letterbox
-from cvu.preprocess.image.general import (basic_preprocess, bgr_to_rgb,
-                                          hwc_to_chw)
+import os
+from typing import Callable, List, Union
+
+import tensorrt as trt
+import pycuda.driver as cuda
+import pycuda.autoinit  # noqa # pylint: disable=unused-import
+
+from cvu.utils.general import read_images_in_batch
 
 
 class Int8EntropyCalibrator2(trt.IInt8EntropyCalibrator2):
+    """Implements trt.IInt8EntropyCalibrator2 for Yolov5.
+    """
     def __init__(
-        self, 
-        batchsize: int = 1, 
-        input_w: int = 1280, 
-        input_h: int = 720, 
-        img_dir: str = None, 
-        calib_cache: str = "int8calib.cache", 
+        self,
+        batchsize: int = 1,
+        input_h: int = 640,
+        input_w: int = 640,
+        img_dir: str = None,
+        preprocess: List[Callable] = None,
+        calib_cache: str = "int8calib.cache",
         ) -> None:
+        """Initialize Int8EntropyCalibrator2.
+
+        Args:
+            batchsize (int): Batchsize for the calibration process.
+            input_h (int): Maximum height of the input for CUDA mem alloc.
+            input_w (int): Maximum width of the input for CUDA mem alloc.
+            img_dir (str): Directory containing calibration images from training dataset.
+            preprocess (List[Callable]): List of preprocessing to apply.
+            calib_cache (str): File to store the calibration cache.
+        """
         trt.IInt8EntropyCalibrator2.__init__(self)
         self.batchsize = batchsize
-        self.total_imgs = 100
         self.input_w = input_w
         self.input_h = input_h
         self.img_dir = img_dir
-        
 
-        # Get all images.
-        self.img_files = [os.path.join(self.img_dir, f) for f in os.listdir(self.img_dir)]
-
-        # check if total image files are enough for calibration
-        if len(self.img_files) < self.total_imgs:
-            raise Exception(
-                "[CVU-Error] Not enough images to perform INT8 calibration !"
-            )
-        
-        self.img_idx = 0
         self.calib_cache = calib_cache
 
-        # Each element of the calibration data is a float32.
+        # each element of the calibration data is a float32
+        # get the larger dim from (h, w)
+        input_dim = input_h if input_h > input_w else input_w
         self.device_input = cuda.mem_alloc(
-            trt.volume((self.batchsize, 3, self.input_w, self.input_w)) * trt.float32.itemsize
+            trt.volume((self.batchsize, 3, input_dim, input_dim)) * trt.float32.itemsize
         )
 
-        self.preprocess = [letterbox, bgr_to_rgb, hwc_to_chw, np.ascontiguousarray, basic_preprocess]
-        self.batches = self.load_batches()
+        self.preprocess = preprocess
 
-    @staticmethod
-    def apply(
-            value: np.ndarray,
-            functions: List[Callable[[np.ndarray], np.ndarray]]):
-        for func in functions:
-            value = func(value)
-        return value
+        self.batches = read_images_in_batch(
+            self.img_dir, self.batchsize, preprocess=self.preprocess
+        )
 
-    def load_batches(self):
-        for i in range(self.total_imgs):
-            print(f"Calib batch {i}")
-            yield self.read_batch_file(self.img_files[self.img_idx:self.img_idx+self.batchsize])
-            self.img_idx = self.img_idx + self.batchsize
-
-    def read_batch_files(self, img_files):
-        batch = list()
-        for img_file in  img_files:
-            # read image
-            img = cv2.imread(img_file)
-
-            # preprocess
-            processed_inputs = self.apply(img, self.preprocess)
-
-            batch.append(processed_inputs)
-        
-        batch = np.array(batch)
-        return batch
-    
     def get_batch_size(self):
+        """Get batch size.
+        """
         return self.batchsize
-    
-    def get_batch(self, names):
+
+    def get_batch(self, names) -> List[int]:    # pylint: disable=unused-argument
+        """Get a batch of input for calibration.
+
+        Args:
+            names: List of file names.
+
+        Returns:
+            A list of device memory pointers set to the memory containing
+            each network input data, or an empty list if there are no more
+            batches for calibration.
+        """
         try:
             # Assume self.batches is a generator that provides batch data.
             data = next(self.batches)
@@ -89,12 +84,24 @@ class Int8EntropyCalibrator2(trt.IInt8EntropyCalibrator2):
             # This signals to TensorRT that there is no calibration data remaining.
             return None
 
-    def read_calibration_cache(self):
-        # If there is a cache, use it instead of calibrating again. Otherwise, implicitly return None.
-        if os.path.exists(self.calib_cache):
-            with open(self.calib_cache, "rb") as f:
-                return f.read()
+    def read_calibration_cache(self) -> Union[memoryview, None]:
+        """Load a calibration cache.
 
-    def write_calibration_cache(self, cache):
-        with open(self.calib_cache, "wb") as f:
-            f.write(cache)
+        Returns:
+            A cache object or None if there is no data.
+        """
+        # If there is a cache, use it instead of calibrating again. Otherwise,
+        # implicitly return None.
+        if os.path.exists(self.calib_cache):
+            with open(self.calib_cache, "rb") as calib_cache_file:
+                return calib_cache_file.read()
+        return None
+
+    def write_calibration_cache(self, cache: memoryview) -> None:
+        """Save a calibration cache.
+
+        Args:
+            cache: The calibration cache to write.
+        """
+        with open(self.calib_cache, "wb") as calib_cache_file:
+            calib_cache_file.write(cache)
