762435dde9b241ecc178c9eba4b918918b12e2b4
shivamswarnkar <shivamswarnkar18@gmail.com>
2022-04-24 20:36:38 +0100

    add basic test structure for cvu.detector.yolov5.core

---
+++ test/unit-tests/test_detector/test_yolov5/test_core.py
@@ -0,0 +1,26 @@
+"""This file contains unit tests for cvu.detector.yolov5.core"""
+from tkinter.messagebox import NO
+from unittest import TestCase
+
+
+class TestYolov5(TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+    
+    def test__call__(self) -> None:
+        pass
+
+    def test__apply(self) -> None:
+        pass
+
+    def test__scale(self) -> None:
+        pass
+
+    def test__load_model(self) -> None:
+        pass
+
+    def test__load_classes(self) -> None:
+        pass
+
+    def test__to_preds(self) -> None:
+        pass
