fa2d69721139ba9c8295f28856bd1e2aee86500a
Alan Turing <alanturing@bluemirrors.org>
2022-07-29 16:35:03 -0700

    add: constructor

--- cvu/image_text_matching/unicl/core.py
+++ cvu/image_text_matching/unicl/core.py
@@ -9,6 +9,7 @@ Find more about UniCL here from their official repository
 https://github.com/microsoft/UniCL
 """
 from importlib import import_module
+from typing import Any
 
 from cvu.interface.core import ICore
 from cvu.utils.backend import setup_backend
@@ -17,7 +18,18 @@ from cvu.utils.backend import setup_backend
 class UniCL(ICore):
     __BACKEND_PKG = "cvu.image_text_matching.unicl.backends"
 
-    def __init__(self):
+    def __init__(self,
+                 backend: str = "torch",
+                 device: str = "auto",
+                 auto_install: bool = False,
+                 **kwargs: Any) -> None:
         # ICore
         super().__init__()
-        pass
+
+        # initiate class attributes
+        self._model = None
+
+        # setup backend and load model
+        if auto_install:
+            setup_backend(backend, device)
+        # self._load_model(backend, device, **kwargs)
