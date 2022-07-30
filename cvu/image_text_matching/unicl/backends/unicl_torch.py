2a38e980c8d2c44237ceb26eca0d9a390d30c2c4
Alan Turing <alanturing@bluemirrors.org>
2022-07-29 17:18:38 -0700

    add: load model

--- cvu/image_text_matching/unicl/backends/unicl_torch.py
+++ cvu/image_text_matching/unicl/backends/unicl_torch.py
@@ -33,3 +33,31 @@ class UniCL(IModel):
                 'cuda:0' if torch.cuda.is_available() else 'cpu')
         else:
             self._device = torch.device(device)
+
+    def _load_model(self, weight: str) -> None:
+        """Internally load torch model
+
+        Args:
+            weight (str): path to torch .pth weight files or predefined-identifiers (such as swin_b, swin_t)
+        """
+        # attempt to load predefined weights
+        if not os.path.exists(weight):
+            if self._device != 'cpu':
+                weight += '.cuda'
+
+            # get path to pretrained weights
+            weight = get_path(__file__, "weights", f"{weight}.pth")
+
+            # download weights if not already downloaded
+            download_weights(weight, "torch")
+
+        # load model
+        self._model = torch.load(weight, map_location=self._device)
+
+        # use FP16 if GPU is being used
+        # TODO - test this
+        if self._device != 'cpu':
+            self._model.half()
+
+        # set model to eval mode
+        self._model.eval()
