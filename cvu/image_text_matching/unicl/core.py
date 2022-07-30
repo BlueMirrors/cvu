8b636a0ad7e530815c3570b837f9ff17eabb0334
Alan Turing <alanturing@bluemirrors.org>
2022-07-29 17:56:43 -0700

    update: unicl core

--- cvu/image_text_matching/unicl/core.py
+++ cvu/image_text_matching/unicl/core.py
@@ -11,20 +11,23 @@ https://github.com/microsoft/UniCL
 from importlib import import_module
 from typing import Any
 
+import numpy as np
+
 from cvu.interface.core import ICore
 from cvu.utils.backend import setup_backend
 
 
 class UniCL(ICore):
-    __BACKEND_PKG = "cvu.image_text_matching.unicl.backends"
+    _BACKEND_PKG = "cvu.image_text_matching.unicl.backends"
 
     def __init__(self,
                  backend: str = "torch",
+                 weight: str = "swin_b",
                  device: str = "auto",
                  auto_install: bool = False,
                  **kwargs: Any) -> None:
         # ICore
-        super().__init__()
+        super().__init__(backend)
 
         # initiate class attributes
         self._model = None
@@ -32,7 +35,7 @@ class UniCL(ICore):
         # setup backend and load model
         if auto_install:
             setup_backend(backend, device)
-        # self._load_model(backend, device, **kwargs)
+        self._load_model(backend, weight, device, **kwargs)
 
     def __repr__(self) -> str:
         """Returns Backend and Model Information
@@ -41,3 +44,15 @@ class UniCL(ICore):
             str: information string
         """
         return str(self._model)
+
+    def _load_model(self, backend: str, weight: str, device: str,
+                    **kwargs: Any) -> None:
+        # load model
+        backend = import_module(f".unicl_{backend}", self._BACKEND_PKG)
+
+        self._model = backend.UniCL(weight, device)
+
+    def __call__(self, inputs: np.ndarray, query: str, **kwargs):
+        # inference on backend
+        image_features, text_features, probs = self._model(
+            inputs, query, **kwargs)
